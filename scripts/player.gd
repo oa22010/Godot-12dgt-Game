c791cf3e256b22f2f1a1c20a4b4b703244ba6c26
oa22010 <opeloge.ahsam@marcellin.school.nz>
2025-08-27 13:27:02 +1200

    Edited player position

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,16 +2,42 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 var score = 0
+var health = 3
 @onready var sword = $Sword
+@onready var label = $"../Label"
+@onready var animation = $AnimatedSprite2D
 
 func _physics_process(_delta: float) -> void:
-	var direction = Input.get_vector("left", "right", "up", "down").normalized()
-	velocity = SPEED * direction
+	var direction = Input.get_vector("left", "right", "up", "down")
+	velocity = SPEED * direction.normalized()
+	if direction == Vector2.LEFT:
+		animation.play("run_left")
+	elif direction == Vector2.RIGHT:
+		animation.play("run_right")
+	elif direction == Vector2.UP:
+		animation.play("run_back")
+	elif direction == Vector2.DOWN:
+		animation.play("run_front")
+	elif direction == Vector2.ZERO:
+		animation.play("idle_front")
 	move_and_slide()
 
 func _on_hitbox_area_entered(area: Area2D) -> void:
+	var direction = Input.get_vector("left", "right", "up", "down")
 	if area is Gold:
 		score += 1
-		sword.num_slashs = score + 1
-		sword.num_slashs = clamp(sword.num_slashs, 1, 12)
 		area.queue_free()
+		sword.num_slashs = score
+		label.text = "Score: " + str(score)
+	elif area is Enemy and health > 0:
+		if direction == Vector2.LEFT:
+			animation.play("hurt_left")
+		elif direction == Vector2.RIGHT:
+			animation.play("hurt_right")
+		elif direction == Vector2.UP:
+			animation.play("hurt_back")
+		elif direction == Vector2.DOWN:
+			animation.play("hurt_front")
+		health -= 1
+	elif area is Enemy and health <= 0:
+		animation.play("death")
