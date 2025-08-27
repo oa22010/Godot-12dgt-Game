dbe3241393a7e1117f00bd741005dfbe9f2a27bc
oa22010 <opeloge.ahsam@marcellin.school.nz>
2025-08-27 13:26:53 +1200

    Hitbox edit
    Edited enemy hitbox

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -3,10 +3,18 @@ extends CharacterBody2D
 const SPEED = 80.0
 var player : Node2D
 var gold_prefab = preload("res://scenes/gold.tscn")
+@onready var animation = $AnimatedSprite2D
 
 func _physics_process(_delta: float) -> void:
-	var direction = (player.global_position - global_position).normalized()
-	velocity = direction * SPEED
+	var direction = (player.global_position - global_position)
+	velocity = SPEED * direction.normalized()
+	var dir = direction.normalized()
+	if dir.x < 0:
+		animation.play("run_left")
+	elif dir.x > 0:
+		animation.play("run_right")
+	else:
+		animation.play("idle_front")
 	move_and_slide()
 
 func _on_hitbox_area_entered(area: Area2D) -> void:
