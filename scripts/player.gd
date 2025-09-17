1c81f063f5c3b24d83700dd69442fee7013dd232
oa22010 <opeloge.ahsam@marcellin.school.nz>
2025-09-18 07:50:12 +1200

    Player update
    Updated player by removing health and making it so upon death the death screen triggers

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,43 +2,42 @@ extends CharacterBody2D
 
 const SPEED = 250.0
 var score = 0
-var health = 3
+@onready var timer = $Timer
 @onready var sword = $Sword
 @onready var label = $Score
-@onready var animation = $AnimatedSprite2D
+@onready var animation_sprite = $AnimatedSprite2D
+@onready var player = $"."
+@onready var death_fade = get_tree().get_root().get_node("DeathFade")
+
+func _onready():
+	timer.stop()
 
 func _physics_process(_delta: float) -> void:
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = SPEED * direction.normalized()
 	if direction == Vector2.LEFT:
-		animation.play("run_left")
+		animation_sprite.play("run_left")
 	elif direction == Vector2.RIGHT:
-		animation.play("run_right")
+		animation_sprite.play("run_right")
 	elif direction == Vector2.UP:
-		animation.play("run_back")
+		animation_sprite.play("run_back")
 	elif direction == Vector2.DOWN:
-		animation.play("run_front")
+		animation_sprite.play("run_front")
 	elif direction == Vector2.ZERO:
-		animation.play("idle_front")
+		animation_sprite.play("idle_front")
 	move_and_slide()
 
 func _on_hitbox_area_entered(area: Area2D) -> void:
-	var direction = Input.get_vector("left", "right", "up", "down")
 	if area is Gold:
 		score += 1
 		area.queue_free()
-		sword.num_slashs = score
-		sword.num_slashs = clamp(sword.num_slashs, 1, 48)
+		sword.num_slashs = clamp(score, 1, 48)
 		label.text = "Score: " + str(score)
-	elif area is Enemy and health > 0:
-		if direction == Vector2.LEFT:
-			animation.play("hurt_left")
-		elif direction == Vector2.RIGHT:
-			animation.play("hurt_right")
-		elif direction == Vector2.UP:
-			animation.play("hurt_back")
-		elif direction == Vector2.DOWN:
-			animation.play("hurt_front")
-		health -= 1
-	elif area is Enemy and health <= 0:
-		animation.play("death")
+	elif area is Enemy:
+		animation_sprite.play("death")
+		Engine.time_scale = 0.1
+		timer.start()
+		death_fade.transition_black()
+
+func _on_timer_timeout() -> void:
+	Engine.time_scale = 0.0
